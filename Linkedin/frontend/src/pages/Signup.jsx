73d251fc0e093c5074d76c5bf19433a1d0359155
rwirub <rwirubs@gmain.com>
2025-04-28 01:48:35 +0530

    initial commit

--- Linkedin/frontend/src/pages/Signup.jsx
+++ Linkedin/frontend/src/pages/Signup.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import logo from "../assets/logo.svg"
 
 function Signup() {
   return (
-    <div>
-      Signup page
+    <div className='w-full h-screen bg-[white]'>
+      <div>
+        <img src ={logo} alt=""></img>
+      </div>
+      
     </div>
   )
 }
